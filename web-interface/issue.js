2b417a40cfe5ae411e1eb709bc36fbc10323c657
Chinmay J Sultanpuri <chinmay.you.know@gmail.com>
2024-10-11 22:37:33 +0530

    Added QR Ticket generator.

--- web-interface/issue.js
+++ web-interface/issue.js
@@ -6,3 +6,48 @@ window.onload = function() {
     document.getElementById("displayStop").innerText += " " + station;
 
 }
+
+function encodeString(inputString) {
+    let encodedStr = btoa(unescape(encodeURIComponent(inputString)));
+    return encodedStr;
+}
+
+
+function generateTicket() {
+    //Issuer Details
+    let user = localStorage.getItem("username");
+    let station = localStorage.getItem("station");
+
+    //Time Details
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    var text = station + "_" + `${hours}:${minutes}:${seconds}` + "_" + document.getElementById("endBusstop").value + "_" + user + "_" + "TI:DB";
+    text = encodeString(text);
+    var qrCodeContainer = document.getElementById("ticketdetails");
+
+    // Clear previous QR code
+    qrCodeContainer.innerHTML = "";
+
+    // Generate new QR code
+    new QRCode(qrCodeContainer, {
+        text: text,
+        width: 128,
+        height: 128
+    });
+
+}
+
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    document.getElementById('displayTime').textContent = `Time:${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000); // Update every second
+updateTime(); // Initial call to display the time right away
